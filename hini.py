df8353ff8fa25313072d5c45ba67a578989d49e3
prem yadav <ppy1699@gmail.com>
2021-03-08 07:22:51 +0530

    hey wnat to push github

--- hini.py
+++
@@ -1,2 +0,0 @@
-
-print(" https://github.com/yprem1612/practical.git")
---
+++ main.py
@@ -0,0 +1,5 @@
+
+ iit push --set-upstream https://github.com/yprem1612/hi.git master
+  print("hii hello") 
+
+  iit push --set-upstream https://github.com/yprem1612/hi.git master
